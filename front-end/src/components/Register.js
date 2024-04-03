1efb0f551273d2243d588826c75c8aac9a107908
diellzane <94406698+diellzan@users.noreply.github.com>
2024-04-03 12:07:52 +0200

    Header, footer and some changes

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const RegisterPage = ({ onRegister }) => {
   const [formData, setFormData] = useState({
@@ -31,23 +32,23 @@ const RegisterPage = ({ onRegister }) => {
                 <div className="register-container" style={{ marginTop: "18px", marginRight: '90px' }}>
                     <h1 className="title" style={{ fontSize: '28px', color: '#003366', marginBottom: '40px', textAlign: 'center' }}>Regjistrohu</h1>
                     <form onSubmit={handleSubmit} className="w-100">
-                        <div className="form-group">
+                        <div className="form-group" style={{ textAlign: 'left' }}>
                             <label htmlFor="firstName" style={{color:'#243b55', fontSize:'22px'}} className="h3 form-label">Emri: </label>
                             <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} className="form-control" required style={{ width: '400px' }} />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group" style={{ textAlign: 'left' }}>
                             <label htmlFor="lastName" style={{color:'#243b55', fontSize:'22px'}} className="h3 form-label mt-3">Mbiemri: </label>
                             <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} className="form-control" required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group" style={{ textAlign: 'left' }}>
                             <label htmlFor="email" className="h3 form-label mt-3" style={{fontSize:'22px'}}>Email: </label>
                             <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="form-control" required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group" style={{ textAlign: 'left' }}>
                             <label htmlFor="password" className="h3 form-label mt-3" style={{fontSize:'22px'}}>Fjalekalimi: </label>
                             <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} className="form-control" required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group" style={{ textAlign: 'left' }}>
                             <label htmlFor="confirmPassword" className="h3 form-label mt-3" style={{fontSize:'22px'}}>Konfirmo fjalekalimin: </label>
                             <input type="password" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} className="form-control" required />
                         </div>
@@ -60,7 +61,7 @@ const RegisterPage = ({ onRegister }) => {
             </div>
         </main>
     </div>
-);
+  );
 }
 
 export default RegisterPage;
