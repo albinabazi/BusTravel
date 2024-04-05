7a9af7922e72d3344e44035002062e486a52ed29
FlakeC <fc52408@ubt-uni.net>
2024-04-06 01:32:21 +0200

    changes

--- front-end/src/components/PassengerDetails.js
+++ front-end/src/components/PassengerDetails.js
@@ -5,25 +5,24 @@ function PassengerDetails() {
         <><div className="title">
             <h1 className="h1" style={{ textDecoration: 'none', color: 'grey', textAlign: 'left', fontFamily: 'sans-serif', marginLeft: '55px', marginTop: '100px', cursor: 'pointer' }}><b>Passenger Details</b></h1>
         </div>
-        <div className="table" style={{ border:'1px solid black', width:'30%', height:'50%', marginLeft:'55px'}}>
-        <table class="table table-condensed-active">
-    <thead>
-      <tr>
-        <th>Passenger Details</th>
-      </tr>
-    </thead>
+        <div className="table" style={{ border:'3px solid black', width:'30%', height:'50%', marginLeft:'55px', overflow: 'hidden', marginTop:'50px'}}>
+        
+        <table class="table table-hover table-condensed-active table-dark" style={{ margin: '0', padding: '0' }} >
     <tbody>
       <tr>
-        <td>Your bookings</td>
+        <td><strong><a href='passengerdetails'class="text-decoration-none text-light" >Passenger Details</a></strong></td>
       </tr>
       <tr>
-        <td>Payment methods</td>
+        <td><a href='yourbookings'class="text-decoration-none text-light">Your bookings</a></td>
       </tr>
       <tr>
-        <td>Notifications</td>
+        <td><a href='paymentmethods'class="text-decoration-none text-light">Payment methods</a></td>
       </tr>
       <tr>
-        <td>Refer a friend</td>
+        <td><a href='notificatios'class="text-decoration-none text-light">Notifications</a></td>
+      </tr>
+      <tr>
+        <td><a href='refertoafriend'class="text-decoration-none text-light">Refer a friend</a></td>
       </tr>
     </tbody>
   </table>
