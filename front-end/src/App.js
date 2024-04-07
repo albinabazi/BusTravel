dd7db9f7a501c291f796341731ade6d94f1b59aa
diellzane <94406698+diellzan@users.noreply.github.com>
2024-04-07 23:04:52 +0200

    Feedback page and others

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,11 @@ import Login from './components/Login';
 import Register from "./components/Register";
 import PassengerDetails from './components/PassengerDetails';
 import Moti from './components/Moti';
+import FeedbackPage from './components/FeedbackPage';
+import Feedback from './components/Feedback';
+import AllFeedbacks from './components/AllFeedbacks';
+import AddFeedback from './components/AddFeedback';
+import EditFeedback from './components/EditFeedback';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -17,7 +22,12 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />     
                     <Route path="/passengerDetails" element={<PassengerDetails />} />     
-                    <Route path='/weather' element={<Moti />} />     
+                    <Route path='/weather' element={<Moti />} />  
+                    <Route path='/feedbackpage' element={FeedbackPage} /> 
+                    <Route path="/AddFeedback" element={AddFeedback} />
+                    <Route path="/Feedback" element={Feedback} />
+                    <Route path="/editFeedback/:id" element={EditFeedback} /> 
+                    <Route path="/AllFeedbacks" element={AllFeedbacks} />  
                 </Routes>
                 <Footer />
             </Router>
---
+++ front-end/src/components/EditFeedback.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+
+
+    
+const EditFeedback = (props) => {
+  const [formData, setFormData] = useState({});
+  const { id } = useParams();
+  console.log(props);
+
+  const GetFeedbackById = () => {
+    const url = `https://localhost:3000/api/feedback/${id}`;
+    fetch(url, {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((feedbacksFromServer) => {
+        console.log(feedbacksFromServer);
+        setFormData(feedbacksFromServer);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(GetFeedbackById, [id]);
+
+  const handleidChange = (e) => {
+    setFormData({
+      ...formData,
+      id: e.target.value,
+    });
+  };
+
+
+  const handleTextChange = (e) => {
+    setFormData({
+      ...formData,
+      text: e.target.value,
+    });
+  };
+
+  const handleCNameChange = (e) => {
+    setFormData({
+      ...formData,
+      companyName: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const feedbackToEdit = {
+      id: id,
+      text: formData.text,
+      companyName: formData.companyName
+    };
+
+    const url = `https://localhost:3000/api/feedback/${id}`;
+
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(feedbackToEdit),
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log(response);
+        alert('Feedback është edituar me sukses!');
+        window.location.href = "/feedback"; // Removed redundant curly braces
+      } else {
+        throw new Error('Failed to update feedback.');
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      alert('Ky feedback nuk mund te editohet!');
+      window.location.href = "/feedback"; // Removed redundant curly braces
+    });    
+}
+
+
+  return (
+
+    
+    <div style={{ marginRight: "33%", paddingLeft: "30%" }}>
+      <form className="w-100 px-5" action="">
+        <h1 className="mt-5">Edito Feedback-un</h1>
+
+
+        <div className="mt-4">
+          <label className="h3 form-label">ID</label>
+          <input
+            value={id}
+            name="name"
+            type="text"
+            className="form-control"
+            onChange={handleidChange}
+          />
+        </div>
+
+
+        <div className="mt-4">
+          <label className="h3 form-label">Teksti</label>
+          <input
+            value={formData.text}
+            name="name"
+            type="text"
+            className="form-control"
+            onChange={handleTextChange}
+          />
+        </div>
+
+        <div className="mt-4">
+          <label className="h3 form-label">Emri i Kompanisë</label>
+          <input
+            value={formData.companyName}
+            name="phonenumber"
+            type="text"
+            className="form-control"
+            onChange={handleCNameChange}
+          />
+        </div>
+
+        <button onClick={handleSubmit} className="btn btn-dark btn-lg w-100 mt-5">Edito</button>
+        <Link to="/feedback" className="btn btn-secondary btn-lg w-100 mt-3">Kthehu mbrapa</Link>
+      </form> <br></br> <br></br>
+    </div>
+  );
+}
+
+export default EditFeedback;
