106b633fa6aad4da3bfb226ce86ae585e29736e4
FatbardhaSylejmani <fatbardha.sy24@gmail.com>
2024-04-07 02:28:26 +0200

    table fixed

--- front-end/src/components/PassengerDetails.js
+++ front-end/src/components/PassengerDetails.js
@@ -1,34 +1,14 @@
+// App.js
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-function PassengerDetails() {
+import Tabela from './Tabela'; // Importing Tabela
+
+const App = () => {
     return (
-        <><div className="title">
-            <h1 className="h1" style={{ textDecoration: 'none', color: 'grey', textAlign: 'left', fontFamily: 'sans-serif', marginLeft: '55px', marginTop: '100px', cursor: 'pointer' }}><b>Passenger Details</b></h1>
+        <div>
+            <h1>Hello, World!</h1>
+            <Tabela /> {/* Using MyComponent */}
         </div>
-        <div className="table" style={{ border:'3px solid black', width:'30%', height:'50%', marginLeft:'55px', overflow: 'hidden', marginTop:'50px'}}>
-        
-        <table class="table table-hover table-condensed-active table-dark" style={{ margin: '0', padding: '0' }} >
-    <tbody>
-      <tr>
-        <td><strong><a href='passengerdetails'class="text-decoration-none text-light" >Passenger Details</a></strong></td>
-      </tr>
-      <tr>
-        <td><a href='yourbookings'class="text-decoration-none text-light">Your bookings</a></td>
-      </tr>
-      <tr>
-        <td><a href='paymentmethods'class="text-decoration-none text-light">Payment methods</a></td>
-      </tr>
-      <tr>
-        <td><a href='notificatios'class="text-decoration-none text-light">Notifications</a></td>
-      </tr>
-      <tr>
-        <td><a href='refertoafriend'class="text-decoration-none text-light">Refer a friend</a></td>
-      </tr>
-    </tbody>
-  </table>
-    </div></>
-
     );
-}
+};
 
-export default PassengerDetails;
+export default App;
